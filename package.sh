897fe9f4803c5218ee2ff2ca7898e763223b046d
Noam Yorav-Raphael <noamraph@gmail.com>
2020-04-13 00:06:48 +0300

    Remove the package.sh script from the zip it creates

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ cd $DIR
 
 FN=/tmp/codemonkey-hebrew-translate.zip
 git archive --format zip --output $FN master
-zip -q -d $FN '*.png' '*.svg'
+zip -q -d $FN '*.png' '*.svg' package.sh
 echo "Package is ready at $FN"
 
