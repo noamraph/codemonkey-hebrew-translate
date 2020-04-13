cbcb54499556bb34b1f2d72262b6dda45889e99c
Noam Yorav-Raphael <noamraph@gmail.com>
2020-04-13 22:36:54 +0300

    Oops, I removed monkey128.png, which is in the manifest.

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ cd $DIR
 
 FN=/tmp/codemonkey-hebrew-translate.zip
 git archive --format zip --output $FN master
-zip -q -d $FN '*.png' '*.svg' package.sh
+zip -q -d $FN screenshot.png monkey.svg package.sh
 echo "Package is ready at $FN"
 
