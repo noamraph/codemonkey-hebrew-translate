9531c312c497750279c4e8900cde8eb041da2ff1
Noam Yorav-Raphael <noamraph@gmail.com>
2020-11-30 11:40:13 +0200

    Change manifest to operate on all of app.codemonkey.com. Support translations for game buttons

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,5 @@
 function main() {
-    const editor = document.querySelector(".ace_editor").env.editor;
-
-    editor.container.addEventListener("contextmenu", function(e) {
-        e.preventDefault();
-        const range = editor.selection.getWordRange();
-        const s = editor.getValue();
-        const lines = s.split('\n');
-        console.assert(range.start.row === range.end.row);
-        const word0 = lines[range.start.row].slice(range.start.column, range.end.column);
-        const word = word0.split('[')[0].split('(')[0];
-        if (!/^[a-zA-Z_]+$/.test(word)) {
-            // Not a word, ignore
-            return false;
-        }
+    function translateWord(word) {
         const audio = new Audio('https://noamraph.github.io/codemonkey-hebrew-translations/' + word + '.mp3');
         audio.play().catch(e => {
             console.log("Didn't find word " + word + ".");
@@ -27,8 +14,32 @@ function main() {
             const audioErr = new Audio('https://noamraph.github.io/codemonkey-hebrew-translations/error.mp3');
             audioErr.play();
         });
+    }
+
+    const editor = document.querySelector(".ace_editor").env.editor;
+    editor.container.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+        const range = editor.selection.getWordRange();
+        const s = editor.getValue();
+        const lines = s.split('\n');
+        console.assert(range.start.row === range.end.row);
+        const word0 = lines[range.start.row].slice(range.start.column, range.end.column);
+        const word = word0.split('[')[0].split('(')[0];
+        if (!/^[a-zA-Z_]+$/.test(word)) {
+            // Not a word, ignore
+            return false;
+        }
+        translateWord(word);
         return false;
     }, false);
+
+    const buttons = document.querySelectorAll(".game-method");
+    buttons.forEach(elem => elem.addEventListener("contextmenu", function(e) {
+        e.preventDefault(); 
+        translateWord(elem.innerText.trim());
+        return false;
+    }, false));
+
 }
 
 const script = document.createElement('script');
